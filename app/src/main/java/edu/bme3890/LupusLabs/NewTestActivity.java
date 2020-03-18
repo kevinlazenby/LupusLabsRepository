fd5a4d825f72da09ebcf705ffb46088fdb383717
vastolagit <56359312+vastolagit@users.noreply.github.com>
2020-03-17 21:53:25 -0500

    Ambient temperature sensor on the "new test" page. Needs units and maybe a conversion from celsius to fahrenheit. Happy Testing

--- app/src/main/java/edu/bme3890/LupusLabs/NewTestActivity.java
+++ app/src/main/java/edu/bme3890/LupusLabs/NewTestActivity.java
@@ -2,13 +2,51 @@ package edu.bme3890.LupusLabs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class NewTestActivity extends AppCompatActivity {
+public class NewTestActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private Sensor mTemperature;
+    TextView sensorTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_test);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mTemperature = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+
+        sensorTextView = (TextView) findViewById(R.id.sensorTextView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, mTemperature, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float ambient_temperature = event.values[0];
+        sensorTextView.setText(Float.toString(ambient_temperature));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Do something here if sensor accuracy changes.
     }
 }
